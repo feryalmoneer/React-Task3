9890d91d2f6f6818743a073463b167e51a4cdcac
feryalmoneer <fryaljaber12@gmail.com>
2024-02-23 21:12:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './Navbar'
+import About from './About'
+import Footer from './Footer'
+
+
+function App() {
+  return (
+    <>
+     <Navbar />
+      <About />
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
